453753636b1256c6fc963ca95afe90abcf2e4bbd
robert <mw.robert@gmail.com>
2022-09-30 11:38:31 +0200

    Task #9 - Tax Calculator - initial setup

---
+++ src/main/java/pl/futurecollars/invoice/service/TaxCalculatorService.java
@@ -0,0 +1,9 @@
+package pl.futurecollars.invoice.service;
+
+public class TaxCalculatorService {
+
+  public TaxCalculatorResult getTaxCalculatorResult(String taxIdentificationNumber) {
+    return new TaxCalculatorResult();
+  }
+
+}
