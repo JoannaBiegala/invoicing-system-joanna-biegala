d27ecb69c0d871a38c1b2a704b8a6c36739e4b70
robert <mw.robert@gmail.com>
2022-10-04 11:35:24 +0200

    Task #10 - Extending Tax Calculator: - extended TaxCalculatorService to take under consideration car related expenses when calculating costs and outgoingVat

--- src/main/java/pl/futurecollars/invoice/service/TaxCalculatorService.java
+++ src/main/java/pl/futurecollars/invoice/service/TaxCalculatorService.java
@@ -1,6 +1,8 @@
 package pl.futurecollars.invoice.service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -38,24 +40,52 @@ public class TaxCalculatorService {
     return database.visit(sellerPredicate(taxIdentificationNumber), this::getTotalPrice);
   }
 
-  private BigDecimal calculateCosts(String taxIdentificationNumber) {
-    return database.visit(buyerPredicate(taxIdentificationNumber), this::getTotalPrice);
-  }
-
   private BigDecimal getTotalPrice(InvoiceEntry entry) {
     return entry.getNetPrice().multiply(entry.getQuantity());
+  }
+
+  private BigDecimal calculateCosts(String taxIdentificationNumber) {
+    return database.visit(buyerPredicate(taxIdentificationNumber), calculateTotalCosts());
+  }
+
+  private Function<InvoiceEntry, BigDecimal> calculateTotalCosts() {
+    return entry -> {
+      if (entry.getExpenseRelatedToCar() != null && entry.getExpenseRelatedToCar().isPersonalUse()) {
+        return calculateTotalNetPrice(entry);
+      }
+      return getTotalPrice(entry);
+    };
+  }
+
+  private BigDecimal calculateTotalNetPrice(InvoiceEntry entry) {
+    return (entry.getNetPrice().add(entry.getVatValue().divide(BigDecimal.valueOf(2), 2, RoundingMode.CEILING)))
+        .multiply(entry.getQuantity());
   }
 
   private BigDecimal calculateIncomingVat(String taxIdentificationNumber) {
     return database.visit(sellerPredicate(taxIdentificationNumber), this::getTotalVat);
   }
 
-  private BigDecimal calculateOutgoingVat(String taxIdentificationNumber) {
-    return database.visit(buyerPredicate(taxIdentificationNumber), this::getTotalVat);
-  }
-
   private BigDecimal getTotalVat(InvoiceEntry entry) {
     return entry.getVatValue().multiply(entry.getQuantity());
+  }
+
+  private BigDecimal calculateOutgoingVat(String taxIdentificationNumber) {
+    return database.visit(buyerPredicate(taxIdentificationNumber), vatEntryToAmount());
+  }
+
+  private Function<InvoiceEntry, BigDecimal> vatEntryToAmount() {
+    return entry -> {
+      if (entry.getExpenseRelatedToCar() != null && entry.getExpenseRelatedToCar().isPersonalUse()) {
+        return calculateTotalVatValue(entry);
+      }
+      return getTotalVat(entry);
+    };
+  }
+
+  private BigDecimal calculateTotalVatValue(InvoiceEntry entry) {
+    return (entry.getVatValue().divide(BigDecimal.valueOf(2), 2, RoundingMode.FLOOR))
+        .multiply(entry.getQuantity());
   }
 
   private Predicate<Invoice> sellerPredicate(String taxIdentificationNumber) {
