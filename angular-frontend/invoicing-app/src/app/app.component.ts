cc1d168dc74b8d041be1da634127efdc63f7723e
Joanna Biegala <joannabiegala0511@gmail.com>
2022-10-20 19:41:10 +0200

    Initialize new Angular project

--- angular-frontend/invoicing-app/src/app/app.component.ts
+++ angular-frontend/invoicing-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Company} from "./company"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'invoicing-app';
+  companies = [
+  {
+  "name": "first",
+  "taxIdentificationNumber": "tax",
+  "address": "aaa",
+  "pensionInsurance": 22.3,
+  "healthInsurance": 33.7
+  },
+ {
+   "name": "second",
+   "taxIdentificationNumber": "tax2",
+   "address": "aaa2",
+   "pensionInsurance": 222.3,
+   "healthInsurance": 233.7
+   }
+  ]
 }
