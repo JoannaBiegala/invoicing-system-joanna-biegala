5522cf1b08e18f4dd973def1aeff648922e82d4c
robert <mw.robert@gmail.com>
2022-10-21 18:26:31 +0200

    Task #17 - Implement companies UI in Angular: - Created form to add new companies and refresh automatically - Added possibility to delete company

--- angular-frontend/invoicing-app/src/app/app.component.ts
+++ angular-frontend/invoicing-app/src/app/app.component.ts
@@ -1,52 +1,45 @@
 import {Component} from '@angular/core';
-import {Company} from "./company"
+import {Company} from './company';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-
-  title: String = 'InvoicingApp';
-
   companies: Company[] = [
     {
-      "name": "first",
-      "taxIdentificationNumber": "tax",
-      "address": "aaa",
-      "pensionInsurance": 22.3,
-      "healthInsurance": 33.7
+      name: 'first',
+      taxIdentificationNumber: 'tax',
+      address: 'aaa',
+      pensionInsurance: 22.3,
+      healthInsurance: 33.7
     },
     {
-      "name": "second",
-      "taxIdentificationNumber": "tax2",
-      "address": "aaa2",
-      "pensionInsurance": 222.3,
-      "healthInsurance": 233.7
+      name: 'second',
+      taxIdentificationNumber: 'tax2',
+      address: 'aaa2',
+      pensionInsurance: 222.3,
+      healthInsurance: 233.7
     }
-  ]
-
+  ];
   newCompany: Company = {
-    "name": "",
-    "taxIdentificationNumber": "",
-    "address": "",
-    "pensionInsurance": 0,
-    "healthInsurance": 0
+    name: '',
+    taxIdentificationNumber: '',
+    address: '',
+    pensionInsurance: 0,
+    healthInsurance: 0
   };
-
   onAddButtonClick() {
     this.companies.push(this.newCompany);
     this.newCompany = {
-      "name": "",
-      "taxIdentificationNumber": "",
-      "address": "",
-      "pensionInsurance": 0,
-      "healthInsurance": 0
+      name: '',
+      taxIdentificationNumber: '',
+      address: '',
+      pensionInsurance: 0,
+      healthInsurance: 0
     };
   }
-
   deleteCompany(company: Company) {
     this.companies = this.companies.filter(c => c !== company);
   }
