6686f2505f62a129ed8c81676cb591e59afba143
robert <mw.robert@gmail.com>
2022-10-21 18:26:31 +0200

    Task #17 - Implement companies UI in Angular: - Created form to add new companies and refresh automatically - Added possibility to delete company

--- angular-frontend/invoicing-app/src/app/app.component.spec.ts
+++ angular-frontend/invoicing-app/src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'invoicing-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('invoicing-app');
+    expect(app.title).toEqual('InvoicingApp');
   });
 
   it('should render title', () => {
--- angular-frontend/invoicing-app/src/app/app.component.ts
+++ angular-frontend/invoicing-app/src/app/app.component.ts
@@ -6,21 +6,49 @@ import {Company} from "./company"
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  companies = [
-  {
-  "name": "first",
-  "taxIdentificationNumber": "tax",
-  "address": "aaa",
-  "pensionInsurance": 22.3,
-  "healthInsurance": 33.7
-  },
- {
-   "name": "second",
-   "taxIdentificationNumber": "tax2",
-   "address": "aaa2",
-   "pensionInsurance": 222.3,
-   "healthInsurance": 233.7
-   }
+
+  title: String = 'InvoicingApp';
+
+  companies: Company[] = [
+    {
+      "name": "first",
+      "taxIdentificationNumber": "tax",
+      "address": "aaa",
+      "pensionInsurance": 22.3,
+      "healthInsurance": 33.7
+    },
+    {
+      "name": "second",
+      "taxIdentificationNumber": "tax2",
+      "address": "aaa2",
+      "pensionInsurance": 222.3,
+      "healthInsurance": 233.7
+    }
   ]
+
+  newCompany: Company = {
+    "name": "",
+    "taxIdentificationNumber": "",
+    "address": "",
+    "pensionInsurance": 0,
+    "healthInsurance": 0
+  };
+
+  onAddButtonClick() {
+    this.companies.push(this.newCompany);
+    this.newCompany = {
+      "name": "",
+      "taxIdentificationNumber": "",
+      "address": "",
+      "pensionInsurance": 0,
+      "healthInsurance": 0
+    };
+  }
+
+  deleteCompany(company: Company) {
+    this.companies = this.companies.filter(c => c !== company);
+  }
+
 }
