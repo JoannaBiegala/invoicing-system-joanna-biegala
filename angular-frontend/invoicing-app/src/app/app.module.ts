8be12924e15700d9867ee1a040729261c46dfb10
robert <mw.robert@gmail.com>
2022-10-24 12:49:32 +0200

    Code cleanup

--- angular-frontend/invoicing-app/src/app/app.module.ts
+++ angular-frontend/invoicing-app/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
