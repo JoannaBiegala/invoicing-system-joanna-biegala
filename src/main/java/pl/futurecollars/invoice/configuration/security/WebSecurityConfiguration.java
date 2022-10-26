589768b411939861022caf94127249aeae4f2410
Kacper Kropidłowski <kappip@tlen.pl>
2022-10-26 11:50:14 +0200

    Spring Security correction

--- src/main/java/pl/futurecollars/invoice/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/pl/futurecollars/invoice/configuration/security/WebSecurityConfiguration.java
@@ -2,26 +2,27 @@ package pl.futurecollars.invoice.configuration.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import pl.futurecollars.invoice.configuration.security.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration {
 
   @Autowired
   private CorsFilter corsFilter;
 
-  @Value("${invoicing-system.csrf.disable:false}")
+  @Value("${invoicing-system.csrf.disable}")
   private boolean disableCsrf;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.httpBasic()
         .and()
         .addFilterBefore(corsFilter, ChannelProcessingFilter.class);
@@ -31,5 +32,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     } else {
       http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
     }
+    return http.build();
   }
 }
