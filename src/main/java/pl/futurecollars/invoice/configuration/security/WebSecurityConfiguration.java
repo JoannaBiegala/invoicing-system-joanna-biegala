5bf18e226f2c06127771029439841a748f733ecd
Joanna Biegala <joannabiegala0511@gmail.com>
2022-10-31 08:54:18 +0100

    security

--- src/main/java/pl/futurecollars/invoice/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/pl/futurecollars/invoice/configuration/security/WebSecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
@@ -25,7 +24,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
-        .antMatchers("/v2/api-docs", "/swagger-resources", "/swagger-ui.html", "/swagger-ui/**","/swagger*/**")
+        .antMatchers("/v2/api-docs", "/swagger-resources", "/swagger-ui.html", "/swagger-ui/**", "/swagger*/**")
         .permitAll()
         .anyRequest()
         .authenticated().and()
